49133f643adf8a36d2380a40f88b134ee5e68070
Tiago Dall'Oca <tdalloca@gmail.com>
2016-12-05 07:06:06 -0200

    Some bugs fixed Atom adoption for refering lines

--- src/editor_clj/core.clj
+++ src/editor_clj/core.clj
@@ -11,49 +11,61 @@
   (str (subs s 0 i) c (subs s (inc i) (.length s))))
 
 (defn line
-  "Returns a map representing a line"
+  "Returns a atom containing map representing a line"
   [s previous-line next-line]
-  {:content s 
-   :next next-line
-   :prev previous-line})
-;; Think I'll use refs
+  (atom {:content s 
+         :next next-line
+         :prev previous-line}))
 
-(defn next
-  [ln]
-  @(:next ln))
+(defn ln-next
+  ([ln]
+   (:next @ln)) 
+  ([ln nln]
+   (reset! (ln-next ln) nln)))
 
-(defn prev
-  [ln]
-  @(:prev ln))
+(defn ln-prev
+  ([ln]
+   (:prev @ln))
+  ([ln nln]
+   (reset! (ln-prev ln) nln)))
+
+(defn ln-content
+  ([ln]
+   (:content @ln))
+  ([ln f & args]
+   (apply swap! (into [ln] args))))
 
 (defn ln-insert-char
   "Inserts c into the line :content at the index i"
   [ln c i] 
-  (update line :content #(str-insert % i c)))
+  (swap! ln update :content #(str-insert % i c)))
 
 (defn ln-replace-char
   "Replaces a char at the index i of line for c"
   [ln c i]
-  (update line :content #(str-replace % i c)))
+  (swap! update ln :content #(str-replace % i c)))
 
 (defn append-line
   "Updates :next field from line"
-  [line nline]
-  (update line :next nline))
+  [ln nln]
+  (swap! update ln :next nln))
 
 (defn append-line-before
   "Updates :prev field from line"
   [ln nln]
-  (update line :prev nline))
+  (swap! update ln :prev nln))
 
 (defn breakline-at
   "Breaks the line at index i and updates :next"
   [ln i]
-  (let [n-str (subs (:content ln) 0 i)
-        b-str (subs (:content ln) i)
-        old-next-line (:next ln)
+  (let [n-str (subs (ln-content ln) 0 i)
+        b-str (subs (ln-content ln) i)
+        old-nln (ln-next ln)
 
-        ln (transient ln)]
-    (do (assoc! ln :content n-str)
-        (let [ln (persistent! ln)]
-          (assoc ln :next (line b-str ln old-next-line))))))
+        ln-c (transient @ln)]
+    (do (assoc! ln-c :content n-str)
+        (assoc! ln-c :next (line b-str ln old-nln))
+        (reset! ln (persistent! ln-c))
+        ln)))
+
+
